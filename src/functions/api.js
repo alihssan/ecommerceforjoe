a655658596a5930014822563ef9d4cd9462e5c1a
Alihssan <alyhassan862@gmail.com>
2021-03-15 00:50:41 +0500

    adde8d

--- src/functions/api.js
+++ src/functions/api.js
@@ -27,14 +27,16 @@ fetch("https://connect.squareup.com/v2/catalog/list", requestOptions)
 // For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
 export function handler(event, context, callback) {
   console.log("queryStringParameters", event.queryStringParameters)
-  var myHeaders = new Headers();
+ /* var myHeaders = new Headers();
 myHeaders.append("Accept", "application/json");
 myHeaders.append("Content-Type", "application/json");
 myHeaders.append("Authorization", "Bearer EAAAEIEcm9qhG1ds8GyWU6SwEcew6_d96hMEAI9QtLa8LRMHByTtcOIZ5-Dj5-8C");
-
+*/
 var requestOptions = {
   method: 'GET',
-  headers: myHeaders,
+  headers: {Accept: "application/json",
+      Content-Type, "application/json",
+      Authorization, "Bearer EAAAEIEcm9qhG1ds8GyWU6SwEcew6_d96hMEAI9QtLa8LRMHByTtcOIZ5-Dj5-8C"},
   redirect: 'follow'
 };
 
@@ -47,7 +49,7 @@ data=data.then(res=>res.text())
     // return null to show no errors
     statusCode: 200, // http status code
     body: JSON.stringify({
-        data
+          
     }),
   })
 }
