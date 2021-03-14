1ea9f1e5e0a2f5928ddac47fb6e55445d66a73e0
Alihssan <alyhassan862@gmail.com>
2021-03-15 01:04:16 +0500

    adde8d

--- src/functions/api.js
+++ src/functions/api.js
@@ -32,15 +32,22 @@ myHeaders.append("Accept", "application/json");
 myHeaders.append("Content-Type", "application/json");
 myHeaders.append("Authorization", "Bearer EAAAEIEcm9qhG1ds8GyWU6SwEcew6_d96hMEAI9QtLa8LRMHByTtcOIZ5-Dj5-8C");
 */
-var requestOptions = {
+/*var requestOptions = {
   method: 'GET',
   headers: {Accept: "application/json",
       Content-Type: "application/json",
       Authorization:"Bearer EAAAEIEcm9qhG1ds8GyWU6SwEcew6_d96hMEAI9QtLa8LRMHByTtcOIZ5-Dj5-8C"},
   redirect: 'follow'
 };
-
-var data=fetch("https://connect.squareup.com/v2/catalog/list", requestOptions);
+*/
+var data=fetch("https://connect.squareup.com/v2/catalog/list", {
+  method: 'GET',
+  headers: {Accept: "application/json",
+      Content-Type: "application/json",
+      Authorization:"Bearer EAAAEIEcm9qhG1ds8GyWU6SwEcew6_d96hMEAI9QtLa8LRMHByTtcOIZ5-Dj5-8C"},
+  redirect: 'follow'
+}
+);
 
 data=data.then(res=>res.text())
   
